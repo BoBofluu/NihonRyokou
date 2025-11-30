7acc2428a455221e730b2c38f7daaf4d7b6a7168
m.li <m.li@recomot.co.jp>
2025-11-30 23:34:32 +0900

    Initial Commit

---
+++ NihonRyokouTests/NihonRyokouTests.swift
@@ -0,0 +1,17 @@
+//
+//  NihonRyokouTests.swift
+//  NihonRyokouTests
+//
+//  Created by m.li on 2025/11/30.
+//
+
+import Testing
+@testable import NihonRyokou
+
+struct NihonRyokouTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
